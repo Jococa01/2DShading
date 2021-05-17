525401097794c179eed13dd96cbd10d189b522a8
Jococa01 <Jococa01@floridauniversitaria.es>
2021-05-17 21:58:50 +0200

    Generación de muro y minimapa

--- Assets/Scripts/FloorAlgorithm.cs
+++ Assets/Scripts/FloorAlgorithm.cs
@@ -12,13 +12,25 @@ public class FloorAlgorithm : MonoBehaviour
     public List<Vector3> HPosiciones = new List<Vector3>();
     public List<int> DireccionesVetadas = new List<int>();
 
-    public int NHabitaciones, Dimensiones, IDHabitación;
-    public Tile Textura1;
+    public int Dimensiones, IDHabitación;
+    public Tile HierbaBase, Muro_d, Muro_U, Muro_R, Muro_L;
     public Tilemap MapaBase, Muro;
     public Tile[] SueloRandom;
     public GameObject AdministradorH, AdministradorCam;
-
     public int CordenadaID = 0;
+
+    [Header("Parámetros modificables en Editor")]
+    [Range(1, 8)]
+    public int NHabitaciones;
+
+    [Header("Parámetros del minimapa")]
+    public Sprite MinimapSprite;
+    public Material UnlitMat;
+    public int RoomHeight, RoomWidth;
+
+    [Header("Generación de Muros")]
+    public List<Vector3> DoorDirections = new List<Vector3>();
+
     void Start()
     {
         for(int n=0; n<NHabitaciones; n++)
@@ -27,6 +39,8 @@ public class FloorAlgorithm : MonoBehaviour
         }
 
         Relocate();
+        MinimapIcons();
+        GenerarMuro();
     }
 
     void GenerarBase()
@@ -50,6 +64,7 @@ public class FloorAlgorithm : MonoBehaviour
         childofobj.AddComponent<TilemapRenderer>();
         childofobj.GetComponent<Tilemap>().tileAnchor = Vector3.zero;
         childofobj.transform.parent = gameobj.transform;
+        childofobj.name = "Suelo";
 
         Tilemap LocalTM = childofobj.GetComponent<Tilemap>();
 
@@ -79,7 +94,7 @@ public class FloorAlgorithm : MonoBehaviour
             for (int x = 0; x > -Dimensiones; x--)
             {
                 Vector3Int Posicion = new Vector3Int(Mathf.FloorToInt(x), Mathf.FloorToInt(Y), 0);
-                LocalTM.SetTile(Posicion, Textura1);
+                LocalTM.SetTile(Posicion, HierbaBase);
             }
         }
         //Generaión del espacio en el 2o cuadrante
@@ -89,7 +104,7 @@ public class FloorAlgorithm : MonoBehaviour
             for (int x = 0; x < Dimensiones; x++)
             {
                 Vector3Int Posicion = new Vector3Int(Mathf.FloorToInt(x), Mathf.FloorToInt(Y), 0);
-                LocalTM.SetTile(Posicion, Textura1);
+                LocalTM.SetTile(Posicion, HierbaBase);
             }
         }
         //Generaión del espacio en el 3er cuadrante
@@ -99,7 +114,7 @@ public class FloorAlgorithm : MonoBehaviour
             for (int x = 0; x < Dimensiones; x++)
             {
                 Vector3Int Posicion = new Vector3Int(Mathf.FloorToInt(x), Mathf.FloorToInt(Y), 0);
-                LocalTM.SetTile(Posicion, Textura1);
+                LocalTM.SetTile(Posicion, HierbaBase);
             }
         }
         //Generaión del espacio en el 4o cuadrante
@@ -109,20 +124,18 @@ public class FloorAlgorithm : MonoBehaviour
             for (int x = 0; x > -Dimensiones; x--)
             {
                 Vector3Int Posicion = new Vector3Int(Mathf.FloorToInt(x), Mathf.FloorToInt(Y), 0);
-                LocalTM.SetTile(Posicion, Textura1);
+                LocalTM.SetTile(Posicion, HierbaBase);
             }
         }
 
         IDHabitación++;
     }
-    void GenerarMuro()
-    {
 
-    }
     void EmebellecerSuelo()
     {
 
     }
+
     void Relocate()
     {
         GameObject[] HArray = Habitaciones.ToArray();
@@ -136,6 +149,8 @@ public class FloorAlgorithm : MonoBehaviour
             }
             else if(n > 0)
             {
+                NuevoLoop:
+
                 int index = RandomInt();
                 Debug.Log(index);
                 Vector3 RelativePos = Vector3.zero;
@@ -169,44 +184,155 @@ public class FloorAlgorithm : MonoBehaviour
                 }
 
                 HArray[n].transform.position = RelativePos;
-                HPosiciones.Add(HArray[n].transform.position);
                 Vector3[] PosiArray = HPosiciones.ToArray();
+
+                int VectorActual = 0;
 
                 foreach(Vector3 hp in PosiArray)
                 {
                     if (hp == HArray[n].transform.position)
                     {
                         Debug.Log("Repetido");
+                        Debug.Log(hp + " es el vector ya existente");
+                        DireccionesVetadas.Add(index);
+                        goto NuevoLoop;
                     }
                     else if(hp != HArray[n].transform.position)
                     {
                         Debug.Log("Nueva Posición");
+                        DireccionesVetadas.Clear();
                     }
+                    VectorActual++;
                 }
+
+                HPosiciones.Add(HArray[n].transform.position);
             }
 
             CArray[n].transform.position = HArray[n].transform.position;
         }
     }
 
-    void RandomDir()
-    {
-
-    }
-
     public int RandomInt()
     {
+        int[] ValoresBase = new int[4] { 1, 2, 3, 4 };
+
+        //Resta de valores según las opciones vetadas
+        int[] ValoresVetados = DireccionesVetadas.ToArray();
+
+        int[] ValorVerdadero = ValoresBase.Where(x => !ValoresVetados.Contains(x)).ToArray();
+
+        int indice = ValoresBase[Random.Range(0, ValorVerdadero.Length)];
+
         //Cambiar esta wea por el Sistema de multiples reintentos en 1 frame, es una mierda pero es lo que hay || Mirar si se pueden quitar X int de un array de ints
 
-        var exclude = new HashSet<int>(DireccionesVetadas);
-        var CustomRange = Enumerable.Range(1, 5).Where(i => !exclude.Contains(i));
+        //var exclude = new HashSet<int>(DireccionesVetadas);
+        //var CustomRange = Enumerable.Range(1, 5).Where(i => !exclude.Contains(i));
 
-        var RandomCoord = new System.Random();
-        int index = RandomCoord.Next(1, 4);
-        return CustomRange.ElementAt(index);
+        //var RandomCoord = new System.Random();
+        //int index = RandomCoord.Next(1, 4);
+        return indice;
     }
-    public void NewDirection()
-    {
 
+    void GenerarMuro()
+    {
+        int Ygen = Dimensiones / 2;
+
+        for(int n=0; n < Habitaciones.ToArray().Length; n++)
+        {
+            GameObject[] HArray = Habitaciones.ToArray();
+            GameObject LayerMuro = new GameObject();
+            LayerMuro.name="Muro";
+            LayerMuro.transform.parent = HArray[n].transform;
+            LayerMuro.AddComponent<Tilemap>();
+            LayerMuro.AddComponent<TilemapRenderer>();
+
+            Tilemap tilemap = LayerMuro.GetComponent<Tilemap>();
+            tilemap.tileAnchor = Vector3.zero;
+
+            //Genero las paredes superiores
+            for(int x=0; x < Dimensiones; x++)
+            {
+                Vector3Int border = new Vector3Int(Mathf.FloorToInt(HArray[n].transform.position.x+x),Mathf.FloorToInt(HArray[n].transform.position.y + Ygen), 0);
+                tilemap.SetTile(border, Muro_d);
+
+                border = new Vector3Int(Mathf.FloorToInt(HArray[n].transform.position.x - x), Mathf.FloorToInt(HArray[n].transform.position.y + Ygen), 0);
+                tilemap.SetTile(border, Muro_d);
+
+                border = new Vector3Int(Mathf.FloorToInt(HArray[n].transform.position.x - x), Mathf.FloorToInt(HArray[n].transform.position.y - Ygen), 0);
+                tilemap.SetTile(border, Muro_U);
+
+                border = new Vector3Int(Mathf.FloorToInt(HArray[n].transform.position.x + x), Mathf.FloorToInt(HArray[n].transform.position.y - Ygen), 0);
+                tilemap.SetTile(border, Muro_U);
+            }
+
+            //Genero las paredes laterales
+            for(int y=0; y<Ygen; y++)
+            {
+                Vector3Int border = new Vector3Int(Mathf.FloorToInt(HArray[n].transform.position.x + Dimensiones), Mathf.FloorToInt(HArray[n].transform.position.y + y), 0);
+                tilemap.SetTile(border, Muro_L);
+
+                border = new Vector3Int(Mathf.FloorToInt(HArray[n].transform.position.x - Dimensiones), Mathf.FloorToInt(HArray[n].transform.position.y + y), 0);
+                tilemap.SetTile(border, Muro_R);
+
+                border = new Vector3Int(Mathf.FloorToInt(HArray[n].transform.position.x - Dimensiones), Mathf.FloorToInt(HArray[n].transform.position.y - y), 0);
+                tilemap.SetTile(border, Muro_R);
+
+                border = new Vector3Int(Mathf.FloorToInt(HArray[n].transform.position.x + Dimensiones), Mathf.FloorToInt(HArray[n].transform.position.y - y), 0);
+                tilemap.SetTile(border, Muro_L);
+            }
+
+            //Genero las esquinas
+            for(int e=0; e<4; e++)
+            {
+                Vector3Int border = new Vector3Int(Mathf.FloorToInt(HArray[n].transform.position.x - Dimensiones), Mathf.FloorToInt(HArray[n].transform.position.y + Ygen), 0);
+                if (e == 0)
+                {
+                    tilemap.SetTile(border, Muro_L);
+                }
+                if (e == 1)
+                {
+                    border = new Vector3Int(Mathf.FloorToInt(HArray[n].transform.position.x + Dimensiones), Mathf.FloorToInt(HArray[n].transform.position.y + Ygen), 0);
+                    tilemap.SetTile(border, Muro_L);
+                }
+                if (e == 2)
+                {
+                    border = new Vector3Int(Mathf.FloorToInt(HArray[n].transform.position.x - Dimensiones), Mathf.FloorToInt(HArray[n].transform.position.y - Ygen), 0);
+                    tilemap.SetTile(border, Muro_L);
+                }
+                if (e == 3)
+                {
+                    border = new Vector3Int(Mathf.FloorToInt(HArray[n].transform.position.x + Dimensiones), Mathf.FloorToInt(HArray[n].transform.position.y - Ygen), 0);
+                    tilemap.SetTile(border, Muro_L);
+                }
+            }
+
+
+            //Dimensiones es la variable de la anchura de la habitación, la altura es Dimensiones/2
+
+            //Coso de la generación de puertas
+            //if(n< Habitaciones.ToArray().Length - 1)
+            //{
+            //    Vector3 DoorDir = HArray[n + 1].transform.position - HArray[n].transform.position;
+            //    DoorDirections.Add(DoorDir);
+            //}
+        }
+    }
+
+    public void MinimapIcons()
+    {
+        for(int n=0; n<Habitaciones.ToArray().Length; n++)
+        {
+            GameObject[] Harray= Habitaciones.ToArray();
+            GameObject Icon = new GameObject();
+            Icon.transform.parent = Harray[n].transform;
+            Icon.layer = 9;
+            Icon.transform.localScale = new Vector3(RoomWidth, RoomHeight, 1);
+            Icon.AddComponent<SpriteRenderer>();
+            SpriteRenderer localsprite = Icon.GetComponent<SpriteRenderer>();
+            localsprite.sprite =MinimapSprite;
+            localsprite.material =UnlitMat;
+            Icon.transform.position = HPosiciones[n];
+            Icon.name = "Icon";
+        }
     }
 }
